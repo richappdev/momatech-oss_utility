ce93af7cc31a3dae1f43b1ec5d131b9fa895387f
admin@K3 <reg@moma-tech.com>
2016-11-10 17:33:22 +0800

    Make it just an easy simple sample

--- bucket.py
+++ bucket.py
@@ -11,7 +11,7 @@ import oss2
 # 通过环境变量获取，或者把诸如“<你的AccessKeyId>”替换成真实的AccessKeyId等。
 access_key_id = os.getenv('OSS_TEST_ACCESS_KEY_ID', 'OOepyKdFufHSV01J')
 access_key_secret = os.getenv('OSS_TEST_ACCESS_KEY_SECRET', 'Tpp4rc2DVWhyah4GNZwri7oFGKpBHu')
-bucket_name = os.getenv('OSS_TEST_BUCKET', 'momatech-image-gallery/pictures/files')
+bucket_name = os.getenv('OSS_TEST_BUCKET', 'momatech-image-gallery')
 endpoint = os.getenv('OSS_TEST_ENDPOINT', 'oss-cn-hangzhou.aliyuncs.com')
 
 # 确认上面的参数都填写正确了
@@ -26,5 +26,3 @@ print('\n'.join(info.name for info in oss2.BucketIterator(service)))
 
 # 创建Bucket对象，所有Object相关的接口都可以通过Bucket对象来进行
 bucket = oss2.Bucket(oss2.Auth(access_key_id, access_key_secret), endpoint, bucket_name)
-
-bucket.put_object('C1661530_posters.jpg', b'content of object')
