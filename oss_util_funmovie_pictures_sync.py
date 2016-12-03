e4c241e482ff80a17b312427204d3f734e036e42
Rich <rich_chang@moma-tech.com>
2016-12-03 21:49:21 +0800

    Fix 檔名為亂碼所造成 UnicodeEncodeError 的例外錯誤

--- oss_util_funmovie_pictures_sync.py
+++ oss_util_funmovie_pictures_sync.py
@@ -98,7 +98,7 @@ for folder in folders:
 		for file in list_diff:
 			if file.endswith('.jpg') or file.endswith('.png'):
 				#if sys.argv[2] is '-detail':
-				print('%d. %s' % (list_diff.index(file)+1, unicode(file).encode("utf-8")))
+				print('%d. %s' % (list_diff.index(file)+1, str(file, encoding='utf-8', errors = 'ignore')))
 			else:
 				list_diff.remove(file)
 	else:
@@ -114,7 +114,7 @@ for folder in folders:
 		
 		for file in list_diff:
 			if sys.argv[2] is '-detail':
-				print(('%d. %s') % (list_diff.index(file)+1, path+unicode(file).encode("utf-8")), end='', flush=True)
+				print(('%d. %s') % (list_diff.index(file)+1, path+str(file, encoding='utf-8', errors = 'ignore')), end='', flush=True)
 			
 			if sys.argv[1] is '-sync':
 				try:
